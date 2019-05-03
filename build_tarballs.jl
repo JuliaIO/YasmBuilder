85486ed8b31199f11663c51a8d865003f6fb6d2c
Ian <i.r.butterworth@gmail.com>
2019-05-02 21:21:09 -0400

    CCLD_FOR_BUILD="$CC"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -16,7 +16,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd yasm-1.3.0/
-./configure --prefix=$prefix --host=$target
+./configure --prefix=$prefix --host=$target CCLD_FOR_BUILD="$CC"
 make -j${nproc}
 make install
 
