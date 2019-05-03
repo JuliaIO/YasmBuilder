6be08428911e3c8a5ad1f96721ff07a10fd905b3
Ian <i.r.butterworth@gmail.com>
2019-05-02 22:51:25 -0400

    alt clean

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -16,10 +16,11 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd yasm-1.3.0/
-make clean
 ./configure --prefix=$prefix --host=$target CCLD_FOR_BUILD="$CC"
 make -j${nproc}
 make install
+make distclean
+make clean
 
 """
 
