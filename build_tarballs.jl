17188b914d2ae929f0a56596c6977b965538202d
Ian <i.r.butterworth@gmail.com>
2019-05-02 20:54:37 -0400

    remove cc=GCC and fix new line

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -16,7 +16,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd yasm-1.3.0/
-CC=gcc ./configure --prefix=$prefix --host=$target
+./configure --prefix=$prefix --host=$target
 make -j${nproc}
 make install
 
